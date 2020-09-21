acaa6c94f01bce8a3cfd752dc1b0f48f40e7bda5
Ayaka Neko <neko@ayaka.moe>
2020-09-21 23:07:05 +0800

    :sparkles: Added Login and Signup page
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

---
+++ frontend/src/pages/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Header />
+    <div class="login-container">
+      <div class="login-title">Login with Email</div>
+      <div class="login-input-container">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Header from '@/components/Header.vue'
+
+export default {
+  components: {
+    Header
+  },
+  created () {
+    document.title = 'Login'
+  }
+}
+</script>
