8297c78b54426c4800350b4500505df559a2d333
Ayaka Neko <neko@ayaka.moe>
2020-09-21 23:06:46 +0800

    :sparkles: Added Link Input
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

---
+++ frontend/src/components/LinkInput.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="link-input-container">
+    <input
+      type="text"
+      id="link"
+      required
+      v-model="link"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      link: ''
+    }
+  },
+  watch: {
+    link (val) {
+      this.$emit('input', val)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.link-input-container {
+  padding: 0 20px;
+  display: flex;
+  align-items: center;
+}
+
+#link {
+  width: 70%;
+  margin: 0 auto;
+  height: 40px;
+  border-radius: 25px;
+  margin-top: 50px;
+  border: 3px solid #48a2e2;
+  outline: none;
+  padding: 0 15px;
+}
+</style>
