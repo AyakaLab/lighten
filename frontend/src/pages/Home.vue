b2ea7da5fdb3ba9f0a73f1537463de5e0f8fd834
Ayaka Neko <neko@ayaka.moe>
2020-09-21 23:07:15 +0800

    :sparkles: Added Home page
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <Header />
+    <LinkInput v-model="linkInput" />
+    <div class="btn-group">
+      <div>
+        <el-button type="primary" round>Shorten Link</el-button>
+        <el-button type="info" round>Options</el-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Header from '@/components/Header.vue'
+import LinkInput from '@/components/LinkInput.vue'
+
+export default {
+  components: {
+    Header,
+    LinkInput
+  },
+  data () {
+    return {
+      linkInput: ''
+    }
+  },
+  created () {
+    document.title = 'Lighten Link Shortener'
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.btn-group {
+  margin-top: 20px;
+  text-align: center;
+}
+</style>
