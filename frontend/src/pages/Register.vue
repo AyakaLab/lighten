c06e9f5713eca6db7713e559ec39d5cb6b49d0ee
Ayaka Neko <neko@ayaka.moe>
2020-10-07 00:56:29 +0800

    :sparkles: Finalize register page
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- frontend/src/pages/Register.vue
+++ frontend/src/pages/Register.vue
@@ -8,17 +8,21 @@
           <el-form-item prop="email">
             <el-input v-model="ruleForm.email" type="email" placeholder="Email" class="input-element"></el-input>
           </el-form-item>
+          <el-form-item prop="code" class="verify-code">
+            <el-input v-model="ruleForm.code" type="number" placeholder="Verification Code" class="input-element verify-input" @input="digitLimit"></el-input>
+            <el-button type="primary" plain class="verify-btn" @click="sendCode">Send Code</el-button>
+          </el-form-item>
           <el-form-item prop="password">
             <el-input v-model="ruleForm.password" type="password" placeholder="Password" class="input-element"></el-input>
           </el-form-item>
-          <el-form-item prop="password">
-            <el-input v-model="ruleForm.passwordRepeat" type="password" placeholder="Repeat Password" class="input-element"></el-input>
+          <el-form-item prop="passwordRepeat">
+            <el-input v-model="ruleForm.passwordRepeat" type="password" placeholder="Confirm Password" class="input-element"></el-input>
           </el-form-item>
           <div class="input-element">
             <span>Already have an account? <router-link :to="{ name: 'Login' }">Login</router-link></span>
           </div>
           <div class="input-element btn-group">
-            <el-button type="primary" plain>Register</el-button>
+            <el-button type="primary" plain @click="register">Register</el-button>
           </div>
         </el-form>
       </div>
@@ -28,16 +32,39 @@
 
 <script>
 
+import Axios from 'axios'
+
 import Header from '@/components/Header.vue'
+import config from '../../../config.json'
 
 export default {
   components: {
     Header
   },
   data () {
+    const validatePass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('Please input password'))
+      } else {
+        if (this.ruleForm.passwordRepeat !== '') {
+          this.$refs.ruleForm.validateField('passwordRepeat')
+        }
+        callback()
+      }
+    }
+    const validatePass2 = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('Please input same password again'))
+      } else if (value !== this.ruleForm.password) {
+        callback(new Error('Password does not match!'))
+      } else {
+        callback()
+      }
+    }
     return {
       ruleForm: {
         email: '',
+        code: '',
         password: '',
         passwordRepeat: ''
       },
@@ -46,16 +73,40 @@ export default {
           { required: true, message: 'Email Address is reuqired', trigger: 'change' },
           { type: 'email', message: 'Please input valid email address', trigger: ['blur', 'change'] }
         ],
+        code: [
+          { required: true, message: 'Verify Code is reuqired', trigger: 'change' }
+        ],
         password: [
           { required: true, message: 'Password is required', trigger: 'blur' },
+          { validator: validatePass, trigger: 'blur' },
+          { min: 8, message: 'Password must be at least 8 characters', trigger: 'blur' }
+        ],
+        passwordRepeat: [
+          { required: true, message: 'Confirm Password is required', trigger: 'blur' },
+          { validator: validatePass2, trigger: 'blur' },
           { min: 8, message: 'Password must be at least 8 characters', trigger: 'blur' }
         ]
       }
     }
   },
   methods: {
-    login () {
-
+    digitLimit (value) {
+      if (this.ruleForm.code.length > 6) {
+        this.ruleForm.code = this.ruleForm.code.slice(0, 6)
+      }
+    },
+    sendCode () {
+      Axios.post(config['api-server'] + '/register/code', { action: 'postCode' })
+    },
+    register () {
+      this.$refs.ruleForm.validate((valid) => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
     }
   },
   created () {
@@ -82,7 +133,48 @@ export default {
   font-weight: 500;
 }
 
+.verify-code {
+  /deep/ .el-form-item__content {
+    display: flex;
+    flex-direction: row;
+    .verify-input {
+      /deep/ input {
+        border-right: none;
+        border-top-right-radius: 0px;
+        border-bottom-right-radius: 0px;
+        &::-webkit-outer-spin-button,
+        &::-webkit-inner-spin-button {
+          -webkit-appearance: none;
+          margin: 0;
+        }
+
+        /* Firefox */
+        &[type=number] {
+          -moz-appearance: textfield;
+        }
+      }
+    }
+    .verify-btn {
+      border-top-left-radius: 0px;
+      border-bottom-left-radius: 0px;
+    }
+  }
+}
+
 .btn-group {
   margin-top: 10px;
+}
+@media screen and (max-width: 1200px) {
+}
+@media screen and (max-width: 992px) {
+}
+@media screen and (max-width: 768px) {
+}
+@media screen and (max-width: 640px) {
+  .register-input-container {
+    max-width: 100%;
+  }
+}
+@media screen and (max-width: 480px) {
 }
 </style>
